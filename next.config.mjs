097e203a3407ac4d2c8eec1cfaf2713254359884
Welder Nascimento Fernandes <welderx3@gmail.com>
2026-04-12 01:04:25 -0300

    ci: add CI/CD workflows and update Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -5,16 +5,16 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "api.dicebear.com",
-      },{
+      },
+      {
         protocol: "https",
         hostname: "images.unsplash.com",
-      }
+      },
     ],
     dangerouslyAllowSVG: true,
     contentDispositionType: "attachment",
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
-  output: 'standalone',
 }
 
 export default nextConfig
